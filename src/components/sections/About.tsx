116a43968c113fdef1546f494aefa7f70a761c13
박성현 <vlvk8195@naver.com>
2026-09-03 23:07:40 +0900

    fix: update about component

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,29 +1,32 @@
 import styles from './About.module.css';
 import { about } from '../../data/about';
+import aboutImg from './OIP.jpg'
 
 
 export default function About() {
   return <section id ='about' className={styles.about}>
 
     <h2>About me</h2>
-    <div className={styles.container}></div>
-    <div className={styles.avatar}>사진</div>
+    <div className={styles.container}>
+
+    <img src={aboutImg} rel='사진' className={styles.poto}></img>
+
+    <div className={styles.info}>
     <div className={styles.box}>
     <p><strong>이름: </strong>{about.name}</p>
     </div>
     <div className={styles.box}>
     <p><strong>생년월일: </strong>{about.birthday}</p>
-
     </div>
 
     <div className={styles.box}>
     <p><strong>거주지: </strong>{about.local}</p>
-
     </div>
 
     <div className={styles.box}>
     <p><strong>학력: </strong>{about.school}</p>
-
+    </div>
+    </div>
     </div>
   </section>;
 }
