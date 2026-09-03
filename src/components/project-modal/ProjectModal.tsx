924076d943be8a85430680be244b85252758d801
park-02 <vlvk8195@naver.com>
2026-09-03 10:55:30 +0900

    구조 변경 및 삭제

--- src/components/project-modal/ProjectModal.tsx
+++ src/components/project-modal/ProjectModal.tsx
@@ -1,11 +1,9 @@
 import { projects } from '../../data/projects';
-import ProjectSideNav from './ProjectSideNav';
 import styles from './ProjectModal.module.css';
 
 export default function ProjectModal() {
   return (
     <div className={styles.modal} data-count={projects.length}>
-      <ProjectSideNav />
     </div>
   );
 }
