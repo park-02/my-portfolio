7fd2fe2b94e16626d501ffd32b910da3408e04af
박성현 <vlvk8195@naver.com>
2026-09-06 12:05:08 +0900

    refactor: About 섹션 UI 개선 및 코드 주석 추가

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -2,6 +2,13 @@ import styles from './About.module.css';
 import { about } from '../../data/about';
 import aboutImg from './OIP.jpg'
 
+// 화면에 보여줄 프로필 정보 (라벨/값)
+const INFO_ITEMS = [
+  { label: '이름', value: about.name },
+  { label: '생년월일', value: about.birthday },
+  { label: '거주지', value: about.local },
+  { label: '학력', value: about.school },
+];
 
 export default function About() {
   return <section id ='about' className={styles.about}>
@@ -12,21 +19,17 @@ export default function About() {
     <img src={aboutImg} rel='사진' className={styles.poto}></img>
 
     <div className={styles.info}>
-    <div className={styles.box}>
-    <p><strong>이름: </strong>{about.name}</p>
-    </div>
-    <div className={styles.box}>
-    <p><strong>생년월일: </strong>{about.birthday}</p>
-    </div>
-
-    <div className={styles.box}>
-    <p><strong>거주지: </strong>{about.local}</p>
-    </div>
-
-    <div className={styles.box}>
-    <p><strong>학력: </strong>{about.school}</p>
-    </div>
-    </div>
+          {/* INFO_ITEMS 배열을 순회하며 각 항목을 박스 UI로 생성 */}
+          {INFO_ITEMS.map((item) => (
+            // React 리스트 렌더링 최적화를 위해 고유한 label을 key로 할당
+            <div key={item.label} className={styles.box}>
+              <p>
+                <strong>{item.label}: </strong>
+                {item.value}
+              </p>
+            </div>
+          ))}
+        </div>
     </div>
   </section>;
 }
