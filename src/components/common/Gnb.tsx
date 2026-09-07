aa04881cea5e99c81b0b8e84af5e37435e866359
박성현 <vlvk8195@naver.com>
2026-09-07 22:52:59 +0900

    fix: back

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,22 +1,6 @@
-import { useState, useEffect } from "react";
 import styles from "./Gnb.module.css";
 
 export default function Gnb() {
-  const [theme, setTheme] = useState<string>("default");
-
-  // 테마 변경 시 최상위 <html> 태그의 data-theme 속성 업데이트
-  useEffect(() => {
-    if (theme === "default") {
-      document.documentElement.removeAttribute("data-theme");
-    } else {
-      document.documentElement.setAttribute("data-theme", theme);
-    }
-  }, [theme]);
-
-  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setTheme(e.target.value);
-  };
-
   return (
     <header className={styles.gnb}>
       <h1 className={styles.logo}>
@@ -42,23 +26,6 @@ export default function Gnb() {
           </li>
           <li>
             <a href="#archive">Archive</a>
-          </li>
-
-          <li>
-            <div className={styles.box}>
-              {/* value와 onChange를 연결 */}
-              <select
-                value={theme}
-                onChange={handleThemeChange}
-                className={styles.themeSelect}
-              >
-                <option value="default">기본</option>
-                <option value="red">빨간색</option>
-                <option value="orange">주황색</option>
-                <option value="yellow">노란색</option>
-                <option value="green">초록색</option>
-              </select>
-            </div>
           </li>
         </ul>
       </nav>
